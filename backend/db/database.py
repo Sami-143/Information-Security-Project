dab51968fd898c91f5d82b1e84de9556837db7d3
Sami Ullah <samiullahglotar420@gmail.com>
2025-05-04 11:24:08 +0500

    Done frontend

--- backend/db/database.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-MONGO_URL = "mongodb+srv://samiullahglotar420:shbo_7700@data.e4my1f1.mongodb.net/?retryWrites=true&w=majority&appName=Data"
-client = MongoClient(MONGO_URL)
-
-db = client.get_database("UserData")
-user_collection = db.get_collection("users")
--- backend/utils/rate_limiter.py
+++
@@ -1,5 +0,0 @@
-# utils/rate_limiter.py
-
-from slowapi import Limiter
-from slowapi.util import get_remote_address
-limiter = Limiter(key_func=get_remote_address)
